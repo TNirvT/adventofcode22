9a275c6698163c861ac03ef8bc79d5fe57080a10
TNirvT <anti_meson@hotmail.com>
2022-12-06 18:02:39 -0800

    day1, part2

--- aoc01.py
+++ aoc01.py
@@ -5,20 +5,29 @@ def open_txt(filename: str) -> list[str]:
             lines.append(line.rstrip())
     return lines
 
-def calories(lines: list[str]) -> list[int]:
-    calories_list = []
+def get_calories(lines: list[str]) -> list[int]:
+    calories = []
     count = 0
     for line in lines:
         if line:
             count += int(line)
         else:
-            calories_list.append(count)
+            calories.append(count)
             count = 0
-    calories_list.append(count)
-    return calories_list
+    calories.append(count)
+    return calories
+
+def top3_sum(calories: list[int]) -> int:
+    result = 0
+    for i in range(3):
+        tmp = max(calories)
+        result += tmp
+        calories.remove(tmp)
+    return result
 
 if __name__ == "__main__":
     lines = open_txt("aoc01.txt")
     # lines = open_txt("test.txt")
-    calories_list = calories(lines)
-    print("part1: ", max(calories_list))
+    calories = get_calories(lines)
+    print("part1: ", max(calories))
+    print("part2: ", top3_sum(calories))
